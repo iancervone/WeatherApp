1bd0a067981917043ffe14b4e1267f7966d1b977
Ian <ian@Ians-MacBook-Pro.local>
2019-10-15 14:07:16 -0400

    added zipcodeHelper, prior commit added all other helper files, plus weatherVC and weatherTVC

---
+++ Models/Zipcode Helper.swift
@@ -0,0 +1,47 @@
+//
+//  Zipcode Helper.swift
+//  WeatherApp
+//
+//  Created by Ian Cervone on 10/15/19.
+//  Copyright © 2019 Ian Cervone. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import CoreLocation
+enum LocationFetchingError: Error {
+    case error(Error)
+    case noErrorMessage
+}
+class ZipCodeHelper {
+    private init() {}
+    static func getLatLong(fromZipCode zipCode: String, completionHandler: @escaping (Result<(lat: Double, long: Double), Error>) -> Void) {
+        let geocoder = CLGeocoder()
+        DispatchQueue.global(qos: .userInitiated).async {
+            geocoder.geocodeAddressString(zipCode){(placemarks, error) -> Void in
+                DispatchQueue.main.async {
+                    if let placemark = placemarks?.first, let coordinate = placemark.location?.coordinate {
+                        completionHandler(.success((coordinate.latitude, coordinate.longitude)))
+                    } else {
+                        let locationError: LocationFetchingError
+                        if let error = error {
+                            locationError = .error(error)
+                        } else {
+                            locationError = .noErrorMessage
+                        }
+                        completionHandler(.failure(locationError))
+                    }
+                }
+            }
+        }
+    }
+}
+ZipCodeHelper.getLatLong(fromZipCode: "60613") { (result) in
+    switch result {
+    case let .success((lat, long)):
+        print(lat, long)
+    case let .failure(error):
+        print(error)
+    }
+}
